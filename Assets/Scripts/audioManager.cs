8b33544659af4650c0e91dfadcb32556416c52dd
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-03-24 16:49:30 +1100

    click and hover update

---
+++ Assets/Scripts/audioManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class audioManager : MonoBehaviour
+{
+
+    public AudioSource source;
+    public AudioClip hover;
+    public AudioClip click;
+
+
+    public void onHover()
+    {
+        source.PlayOneShot(hover);
+    }
+
+    public void onClick()
+    {
+        source.PlayOneShot(click);
+    }
+
+}
+

362978c7dc30b547a7291e85b03a9c8500f50f34
MyopicMuppet <47373110+MyopicMuppet@users.noreply.github.com>
2019-03-24 17:37:56 +1100

    update

--- Assets/Scripts/audioManager.cs
+++ Assets/Scripts/audioManager.cs
@@ -4,21 +4,23 @@ using System;
 
 public class audioManager : MonoBehaviour
 {
-
+    #region Variables
     public AudioSource source;
     public AudioClip hover;
     public AudioClip click;
-
-
+    #endregion
+    #region Button Sounds
+    //Set Sound on hover over buttons
     public void onHover()
     {
         source.PlayOneShot(hover);
     }
 
+    //Set sound on click buttons
     public void onClick()
     {
         source.PlayOneShot(click);
     }
-
+    #endregion
 }
 
